56ef59e583bf3b1eab649c10841b15310ff736aa
zangwoosung <zangwoosung@gmail.com>
2025-03-24 00:59:10 +0900

    복사본

---
+++ Program - 복사본.cs
@@ -0,0 +1,15 @@
+﻿namespace GitPracticeFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("master, World!");
+        }
+
+        static void Sample()
+        {
+            Console.WriteLine("I am dandan software branch ");
+        }
+    }
+}
